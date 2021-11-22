a00ec6a82ccc86ef1d61d73f998231d7e8a1025d
lmingi <mingilee98@gmail.com>
2021-11-22 02:50:17 -0600

    change theme and edit navbar

--- app.R
+++ app.R
@@ -1,18 +1,43 @@
 library(shiny)
+library(dplyr)
+library(ggplot2)
+library(DT)
+library(ggrepel)
+library(tidyr)
+library(shinycssloaders)
+library(shinythemes)
+
+fiftystatesCAN <- read.csv("/Users/leemingi/Documents/324-ShinyApp/fiftystatesCAN.csv")
+BigTop100 <- read.csv("/Users/leemingi/Documents/324-ShinyApp/gradData.csv")
 
 ui <- fluidPage(
+  # tags$head(
+  #   tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
+  # ),
+  
   navbarPage(
-    title = 'Team PHD',
-    tabPanel('Project Description'),
-    tabPanel('Design Process'),
-    tabPanel('Graduate Program Finder'),
-    tabPanel('Acknowledgements and References'),
-    tabPanel('Reflection')
+    title = 'Team PHD',theme = shinytheme("united"),
+    tabPanel("Graduate Program Finder",
+             HTML("<h1><center><b>Graduate School</b> Finder</center></h1>"),
+             br(), br(),br(), br()),
+
+    tabPanel("Program Comparison",
+             br(), br(),br(), br()),
+    
+    tabPanel("Data Visualization",
+             br(), br(),br(), br()),
+
+    navbarMenu("More",
+               tabPanel('Project Description'),
+               tabPanel('Design Process'),
+               tabPanel('Acknowledgements and References'),
+               tabPanel('Reflection'))
   )
+  
 )
 
 server <- function(input, output){
-  
+
 }
   
 shinyApp(ui = ui, server = server)
